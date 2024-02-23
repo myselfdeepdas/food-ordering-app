3c7e64c81e0d987b7d215ae46e194645eb29b01e
DEEP DAS <deepdassonu.ktr@gmail.com>
2024-02-23 10:40:24 +0530

    Done

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0QxrPXpsQQraf33bC8blmrUgMafyjCO8",
+  authDomain: "food-ordering-3b858.firebaseapp.com",
+  projectId: "food-ordering-3b858",
+  storageBucket: "food-ordering-3b858.appspot.com",
+  messagingSenderId: "812692817785",
+  appId: "1:812692817785:web:30b1ca0338680018f2d1a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
